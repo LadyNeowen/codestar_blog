d2b39a00f45a2696978995123d4e4f094e75861e
Alicia <alicia.westerlund@outlook.com>
2025-11-04 15:23:20 +0100

    Changed code from scource code

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def blog_hello(request):
-    return HttpResponse("Hello, blog!", content_type="text/plain; charset=utf-8")
+# Create your views here.
+def my_blog(request):
+    return HttpResponse("Hello, Blog!")
 # Create your views here.
