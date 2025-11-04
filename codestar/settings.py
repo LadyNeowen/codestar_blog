8da261a3baffa05991092b7af945a8fff085e08b
Alicia <alicia.westerlund@outlook.com>
2025-11-04 20:20:27 +0100

    Adding login for superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000/",
+    "https://whispers-by-alice-77888c980adc.herokuapp.com/"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
