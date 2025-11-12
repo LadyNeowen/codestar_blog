16845a54b85aea0d566d5de77bdd77972fb6a711
Alicia <alicia.westerlund@outlook.com>
2025-11-12 15:17:49 +0100

    Add summernote package

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
@@ -131,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Directory where `collectstatic` will collect static files for deployment.
+# Added temporarily so collectstatic can run locally.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
